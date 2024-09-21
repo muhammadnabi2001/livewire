c913c7e38b0cd9f1d6c7534cf5bc271822776068
= <xoliqulovmuhammadnabi842@gmail.com>
2024-09-21 15:00:00 +0500

    Updated

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -9,7 +9,6 @@
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         @vite('resources/js/app.js')
 </head>
-
 <body>
     <div class="container">
         <div class="row">
